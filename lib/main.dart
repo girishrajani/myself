730d77496497a3a0646fbdf49feeefc8ef034758
Girish Rajani <girish.r.rajani@gmail.com>
2021-01-12 06:20:46 +0530

    AppBar Added

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey[900],
+          title: Text('Girish Rajani'),
+        ),
+      ),
+    ),
+  );
+}
