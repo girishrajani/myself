b2139db93480e9356c1177e18e4a9c79279ba611
Girish Rajani <girish.r.rajani@gmail.com>
2021-01-12 06:35:35 +0530

    Completed the app

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.grey[900],
         appBar: AppBar(
-          backgroundColor: Colors.grey[900],
+          backgroundColor: Colors.black,
           title: Text('Girish Rajani'),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/myself.jpg'),
+          ),
         ),
       ),
     ),
